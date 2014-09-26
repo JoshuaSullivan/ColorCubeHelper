aad486808fb9176240da5cebb90c9dc12d4caff6
Joshua Sullivan <jsulliva@nerdery.com>
2014-09-26 16:46:17 -0500

    Changed the byte order of the images.
    I modified the order in which pixels are written to the image file in
    order to facilitate the conversion to NSData for the CIColorCube
    filter. There should no longer be a need for complicated index
    translation as the order in the image file is the same as in the data
    file.

--- colorCubeImageGenerator/Classes/CCHReferenceImageCreator.m
+++ colorCubeImageGenerator/Classes/CCHReferenceImageCreator.m
@@ -8,35 +8,33 @@
 
 @implementation CCHReferenceImageCreator
 
+// 16, 64, 256
+
 + (NSImage *)createReferenceImageWithCubeSize:(size_t)cubeSize
 {
-    size_t gridSize = (size_t)sqrt(cubeSize);
-    size_t imageDimension = cubeSize * gridSize;
-    size_t componentSize = sizeof(uint8_t);
-    size_t memSize = cubeSize * cubeSize * cubeSize * 4 * componentSize;
+    NSAssert(cubeSize == 16 || cubeSize == 64 || cubeSize == 256, @"ERROR: cubeSize must be one of the following: 16, 64, 256");
+    size_t pixels = cubeSize * cubeSize * cubeSize;
+    size_t imageDimension = (size_t)sqrt(pixels);
+    size_t channels = 4;
+    size_t channelSize = sizeof(uint8_t);
+    size_t memSize = pixels * channels * channelSize;
     uint8_t *bytes = malloc(memSize);
-    float colorStep = 255.0f / (float)(cubeSize - 1);
-    size_t offset = 0;
-    uint8_t r, g, b;
-    for (size_t j = 0; j < imageDimension; j++) {
-        for (size_t i = 0; i < imageDimension; i++) {
-            r = (uint8_t)roundf((i % cubeSize) * colorStep);
-            g = (uint8_t)roundf((j % cubeSize) * colorStep);
-            b = (uint8_t)roundf((i / cubeSize + (j / cubeSize) * gridSize) * colorStep);
-            bytes[offset + 0] = r;
-            bytes[offset + 1] = g;
-            bytes[offset + 2] = b;
-            bytes[offset + 3] = 255U;
-            offset += 4;
-        }
+    size_t cubeSizeSquared = cubeSize * cubeSize;
+    size_t offset;
+    float colorStep = 255.0f / (cubeSize - 1);
+    for (size_t i = 0; i < pixels; i++) {
+        offset = i * channels;
+        bytes[offset + 0] = (uint8_t)roundf((i % cubeSize) * colorStep);
+        bytes[offset + 1] = (uint8_t)roundf(((i / cubeSize) % cubeSize) * colorStep);
+        bytes[offset + 2] = (uint8_t)roundf((i / cubeSizeSquared) * colorStep);
+        bytes[offset + 3] = 255U;
     }
-
     CGDataProviderRef dataProvider = CGDataProviderCreateWithData(NULL, bytes, memSize, NULL);
-    size_t bitsPerComponent = componentSize * 8;
-    size_t bitsPerPixel = 32;
-    size_t bytesPerRow = imageDimension * 4;
+    size_t bitsPerComponent = 8U * channelSize;
+    size_t bitsPerPixel = channels * bitsPerComponent;
+    size_t bytesPerRow = imageDimension * channels;
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaLast;
+    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault | kCGImageAlphaPremultipliedLast;
     CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
     CGImageRef imageRef = CGImageCreate(imageDimension, imageDimension, bitsPerComponent, bitsPerPixel, bytesPerRow, colorSpace, bitmapInfo, dataProvider, NULL, NO, renderingIntent);
     NSImage *cubeImage = [[NSImage alloc] initWithCGImage:imageRef size:CGSizeMake(imageDimension, imageDimension)];
